530afe0c6ce9822475b965d884070df78dd6ac07
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-12-08 13:22:35 +0300

    all change

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,29 +3,21 @@ package ru.netology.sqr;
 public class SQRService {
 
 
-    public int calcSqr(int min , int max) {
-
-for (int i = 10; i <= min;i++){
-
-    if (i * min <= max){
-
-        return i;
-
-
+    public int calcSqr(int min, int max) {
+        int amount = 0;
+        for (int i = 10; i <= 99; i++) {
+            int ii = i * i;
+            if (ii >= min) {
+                if (ii <= max) {
+                    amount++;
+                }
+            }
+        }
+        return amount;
     }
-    else;
 
 }
 
-
-
-
-return 0;
-
-    }
-
-
-    }
 
 
 
--- src/test/java/TESTsqr.java
+++ src/test/java/TESTsqr.java
@@ -1,10 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.sqr.SQRService;
 
 public class TESTsqr {
-    public void test(){
-        SQRService service =new SQRService();
+    @ParameterizedTest
+    @CsvSource({
+            "10,200,5",
+            "100,1000,22",
+            "1000,10000,68"
+    })
+    public void testSquare(int min, int max, int expected) {
+        SQRService service = new SQRService();
 
-        int expected =
+        int actual = service.calcSqr(min, max);
+        Assertions.assertEquals(expected, actual);
+
 
     }
 
